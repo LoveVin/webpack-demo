60d8f87e09b936c1c3dc8dddd1c40177c5e8c46a
twinklelixn <xiaonanli@163.com>
2020-07-01 17:12:50 +0800

    手动合并two-config

---
+++ webpack.config.base.js
@@ -0,0 +1,18 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: '小柴',
+      filename: 'index.html',
+      template: 'src/assets/index.html'
+    })
+  ],
+};
