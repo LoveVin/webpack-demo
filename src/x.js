6ae046b38c41635af7a4ed417381b8e2ecd14b97
twinklelixn <xiaonanli@163.com>
2020-07-01 19:11:31 +0800

    懒加载

--- src/x.js
+++ src/x.js
@@ -10,3 +10,16 @@ const div = document.getElementById("app");
 div.innerHTML = `
     <img src="${img}">
 `
+
+const button = document.createElement("button");
+button.innerHTML = "懒加载";
+button.onclick = () => {
+    const lazyPromise = import('./lazy.js');
+    lazyPromise.then((module)=>{
+        module.default();
+    },()=>{
+        console.log("模块加载错误");
+    })
+}
+
+div.appendChild(button);
