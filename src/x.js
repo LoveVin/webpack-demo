90f33a052a25c4d36adf9b7a36956f2d5128033d
twinklelixn <xiaonanli@163.com>
2020-07-01 19:00:20 +0800

    file-loader

--- src/x.js
+++ src/x.js
@@ -1,4 +1,12 @@
-import x from './x.less'
+import './x.less'
+import './y.styl'
+import img from './assets/logo.png'
 export default 'I am xxx'
 
-console.log(x.toString())
+console.log(img);
+
+const div = document.getElementById("app");
+
+div.innerHTML = `
+    <img src="${img}">
+`
